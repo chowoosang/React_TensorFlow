0927a249a187002b87152a29aea7a80b55a8b1d7
woosang <woosang3078@hanmail.net>
2023-02-01 23:04:25 +0900

    로딩 텍스트 스타일링

--- src/components/bodypix.js
+++ src/components/bodypix.js
@@ -1,75 +1,88 @@
-import '@tensorflow/tfjs';
-import * as bodyPix from '@tensorflow-models/body-pix';
-import { useEffect, useState } from 'react';
-import { useRef } from 'react';
-import RedSpinner from './hooks/spinner';
+import "@tensorflow/tfjs";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
+import RedSpinner from "./hooks/spinner";
+import styled from "styled-components";
 
 const Remove = () => {
-    const canvasRef = useRef(null);
-    const [loading, setLoading] = useState(false);
+  const canvasRef = useRef(null);
+  const [loading, setLoading] = useState(false);
 
-    const loadImage = () => {
-        const img = new Image();
-        img.src = process.env.PUBLIC_URL + '/girl.jpeg'
+  const loadImage = () => {
+    const img = new Image();
+    img.src = process.env.PUBLIC_URL + "/girl.jpeg";
 
-        const ctx = canvasRef.current.getContext('2d');
+    const ctx = canvasRef.current.getContext("2d");
 
-        img.addEventListener('load', () => {
-            canvasRef.current.width = img.width;
-            canvasRef.current.height = img.height;
-            ctx.drawImage(img, 0, 0)
-        })
-    }
+    img.addEventListener("load", () => {
+      canvasRef.current.width = img.width;
+      canvasRef.current.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    });
+  };
 
-    const backgroundRemoval = async () => {
-        setLoading(true);
+  const backgroundRemoval = async () => {
+    setLoading(true);
 
-        const canvas = canvasRef.current;
-        const net = await bodyPix.load({
-            architecture: 'ResNet50',
-            outputStride: 32,
-            quantBytes: 4
-        })
-        const segmentation = await net.segmentPerson(canvas, {
-            internalResolution: 'medium',
-            segmentationThreshold: 0.7,
-            scoreThreshold: 0.7
-        })
-        const ctx = canvas.getContext('2d');
-        const { data: imgData } = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const canvas = canvasRef.current;
+    const net = await bodyPix.load({
+      architecture: "ResNet50",
+      outputStride: 32,
+      quantBytes: 4,
+    });
+    const segmentation = await net.segmentPerson(canvas, {
+      internalResolution: "medium",
+      segmentationThreshold: 0.7,
+      scoreThreshold: 0.7,
+    });
+    const ctx = canvas.getContext("2d");
+    const { data: imgData } = ctx.getImageData(
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
 
-        const newImg = ctx.createImageData(canvas.width, canvas.height)
-        const newImgData = newImg.data;
+    const newImg = ctx.createImageData(canvas.width, canvas.height);
+    const newImgData = newImg.data;
 
-        segmentation.data.forEach((segment, i) => {
-            if (segment === 1) {
-                newImgData[i*4] = imgData[i*4]
-                newImgData[i*4+1] = imgData[i*4+1]
-                newImgData[i*4+2] = imgData[i*4+2]
-                newImgData[i*4+3] = imgData[i*4+3]
-            }
-        })
+    segmentation.data.forEach((segment, i) => {
+      if (segment === 1) {
+        newImgData[i * 4] = imgData[i * 4];
+        newImgData[i * 4 + 1] = imgData[i * 4 + 1];
+        newImgData[i * 4 + 2] = imgData[i * 4 + 2];
+        newImgData[i * 4 + 3] = imgData[i * 4 + 3];
+      }
+    });
 
-        ctx.putImageData(newImg, 0, 0)
-        setLoading(false);
-    }
+    ctx.putImageData(newImg, 0, 0);
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        loadImage();
-    }, [])
+  useEffect(() => {
+    loadImage();
+  }, []);
 
-    return (
-        <div>
-            <canvas ref={canvasRef}/>
-            <div>
-                {loading ? <div>
-                    <h2>로딩중...</h2>
-                    <RedSpinner loading={loading} />
-                </div> : null}
-            </div>
-            <button onClick={backgroundRemoval}>제거</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <canvas ref={canvasRef} />
+      <div>
+        {loading ? (
+          <div>
+            <LoadingText>로딩중...</LoadingText>
+            <RedSpinner loading={loading} size={30} />
+          </div>
+        ) : null}
+      </div>
+      <button onClick={backgroundRemoval}>제거</button>
+    </div>
+  );
+};
+
+const LoadingText = styled.h2`
+  text-align: center;
+`
+
 
 export default Remove;
