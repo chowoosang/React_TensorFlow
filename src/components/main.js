69f40d19a76f5880c10e82e77671e82eae379835
조우상 <woosang3078@hanmail.net>
2023-02-01 12:21:13 +0900

    메인 페이지 스타일 추가

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Main() {
-    return (
-        <div>
-            <Link to="/image-identify">이미지 식별기</Link>
-            <Link to="/delete-background">배경 인식 및 제거기</Link>
-        </div>
-    )
+  return (
+    <Container>
+      <ContainerList>
+        <Link to="/image-identify">이미지 식별기</Link>
+      </ContainerList>
+      <ContainerList>
+        <Link to="/delete-background">배경 인식 및 제거기</Link>
+      </ContainerList>
+    </Container>
+  );
 }
+
+const Container = styled.ul`
+  list-style: none;
+  display: flex;
+`;
+
+const ContainerList = styled.li`
+  padding: 30px;
+`;
