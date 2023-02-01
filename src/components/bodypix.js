7321731b2f690abd245103d5ee8652d273e1ba1e
조우상 <woosang3078@hanmail.net>
2023-02-01 12:13:34 +0900

    배경 이미지 제거 구현 완료

--- src/components/bodypix.js
+++ src/components/bodypix.js
@@ -1,32 +1,72 @@
 import '@tensorflow/tfjs';
-import * as bodyPix from '@tensorflow-models/body-pix'
-import * as fs from 'fs';
-import * as jimp from 'jimp';
-import React, { useState, useEffect, useRef } from 'react';
+import * as bodyPix from '@tensorflow-models/body-pix';
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 
-const BodyPix = () => {
-    const [model, setModel] = useState(false);
-    const [isModelLoading, setIsModelLoading] = useState(null);
-    const [imageURL, setImageURL] = useState(null);
-    const [results, setResults] = useState(null);
 
-    const imageRef = useRef();
+const Remove = () => {
+    const canvasRef = useRef(null);
+    const [loading, setLoading] = useState(false);
 
-    const loadModle = async () => {
-        setIsModelLoading(true);
-        try {
-            if (!bodyModel) {
-                const resNet = {
-                    architecture: 'ResNet50',
-                    outputStride: 16,
-                    qunatBytes: 4
-                };
-                const bodyModel = await bodyPix.load(resNet);
-            }
-        } catch (err) {
-            console.log(err);
-        }
+    const loadImage = () => {
+        const img = new Image();
+        img.src = process.env.PUBLIC_URL + '/girl.jpeg'
+
+        const ctx = canvasRef.current.getContext('2d');
+
+        img.addEventListener('load', () => {
+            canvasRef.current.width = img.width;
+            canvasRef.current.height = img.height;
+            ctx.drawImage(img, 0, 0)
+        })
     }
+
+    const backgroundRemoval = async () => {
+        setLoading(true);
+
+        const canvas = canvasRef.current;
+        const net = await bodyPix.load({
+            architecture: 'ResNet50',
+            outputStride: 32,
+            quantBytes: 4
+        })
+        const segmentation = await net.segmentPerson(canvas, {
+            internalResolution: 'medium',
+            segmentationThreshold: 0.7,
+            scoreThreshold: 0.7
+        })
+        const ctx = canvas.getContext('2d');
+        const { data: imgData } = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+        const newImg = ctx.createImageData(canvas.width, canvas.height)
+        const newImgData = newImg.data;
+
+        segmentation.data.forEach((segment, i) => {
+            if (segment == 1) {
+                newImgData[i*4] = imgData[i*4]
+                newImgData[i*4+1] = imgData[i*4+1]
+                newImgData[i*4+2] = imgData[i*4+2]
+                newImgData[i*4+3] = imgData[i*4+3]
+            }
+        })
+
+        ctx.putImageData(newImg, 0, 0)
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadImage();
+    }, [])
+
+    return (
+        <div>
+            <canvas ref={canvasRef}/>
+            <div>
+                {loading ? <h2>제거중...</h2> : null}
+            </div>
+            <button onClick={backgroundRemoval}>제거</button>
+        </div>
+    )
 }
 
-export default BodyPix;
+export default Remove;
