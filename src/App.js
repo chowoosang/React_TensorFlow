d7580871e29096186b3e36658c0cd4f2cf796087
조우상 <woosang3078@hanmail.net>
2023-02-01 12:13:12 +0900

    라우터 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import BodyPix from "./components/bodypix";
+import Remove from "./components/bodypix";
+import Identify from "./components/identify";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Main from "./components/main";
 
 const App = () => {
   return (
-    <>
-      <BodyPix />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/image-identify" element={<Identify />}></Route>
+        <Route path="/delete-background" element={<Remove />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
