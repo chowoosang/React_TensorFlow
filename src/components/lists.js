d512704ba5c92371a51036cc71136f246a5a5b83
조우상 <woosang3078@hanmail.net>
2023-02-02 11:26:41 +0900

    소개 문구 추가

--- src/components/lists.js
+++ src/components/lists.js
@@ -2,62 +2,63 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Lists() {
-    return (
-        <div>
-            <TopSpace />
-            <Title>Tensorflow.js 모델 기반 딥러닝 기능</Title>
-            <MidSpace />
-            <div>
-                <ModelLists>
-                    <Models>
-                        <ModelContainer>
-                            이미지 판별
-                        </ModelContainer>
-                    </Models>
-                    <Models>
-                        <ModelContainer>
-                            배경 이미지 인식 및 제거
-                        </ModelContainer>
-                    </Models>
-                    <Models>
-                        <ModelContainer>
-                            웹캠 사물 탐지
-                        </ModelContainer>
-                    </Models>
-                </ModelLists>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <TopSpace />
+      <Title>Tensorflow.js 모델 기반 딥러닝 기능</Title>
+      <MidSpace>
+        Explore pre-trained models to add computer vision, natural language
+        processing (NLP), and other common ML tasks to your web and
+        browser-based applications.
+      </MidSpace>
+      <div>
+        <ModelLists>
+          <Models>
+            <ModelContainer>이미지 판별</ModelContainer>
+          </Models>
+          <Models>
+            <ModelContainer>배경 이미지 인식 및 제거</ModelContainer>
+          </Models>
+          <Models>
+            <ModelContainer>웹캠 사물 탐지</ModelContainer>
+          </Models>
+        </ModelLists>
+      </div>
+    </div>
+  );
 }
 
 const MidSpace = styled.div`
-  height: 50px;
-`
+  width: 597.5px;
+  height: 90px;
+  margin: 0 auto;
+  color: #425066;
+`;
 
 const ModelContainer = styled.div`
   width: 268.73px;
   height: 318.11px;
-  border: 1px solid #E6E6E6;
+  border: 1px solid #e6e6e6;
   border-radius: 6px;
-`
+`;
 
 const Models = styled.li`
   list-style: none;
   margin-left: 40px;
-`
+`;
 
 const ModelLists = styled.ul`
   display: flex;
   justify-content: center;
-`
+`;
 
 const Title = styled.p`
   font-size: 33px;
   font-weight: 600;
   text-align: center;
   text-decoration: underline;
-`
+`;
 
 const TopSpace = styled.div`
   height: 85px;
-`
+`;
