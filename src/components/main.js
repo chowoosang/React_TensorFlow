75592c2e639979b8204c1107d150956b9e333b6e
조우상 <woosang3078@hanmail.net>
2023-02-02 10:27:57 +0900

    링크 스타일링

--- src/components/main.js
+++ src/components/main.js
@@ -7,11 +7,24 @@ export default function Main() {
     <div>
       <TopSpace />
       <Title>DeepLearning Projects</Title>
-      <Link to="/lists">딥러닝 기능 보러가기</Link>
+      <LinkContainer>
+        <LinkStyle to="/lists">딥러닝 기능 보러가기</LinkStyle>
+      </LinkContainer>
     </div>
-  )
+  );
 }
 
+const LinkContainer = styled.div`
+  width: 132.94px;
+  height: 18px;
+  margin: 0 auto;
+`;
+
+const LinkStyle = styled(Link)`
+  color: #2F96F0;
+  text-decoration: none;
+  font-size: 16px;
+`;
 
 const easeIn = keyframes`
   0% {
@@ -22,15 +35,15 @@ const easeIn = keyframes`
   100% {
     opacity: 1;
   }
-`
+`;
 
 const Title = styled.p`
   font-size: 40px;
   font-weight: 620;
   text-align: center;
   animation: ${easeIn} 1s ease-in;
-`
+`;
 
 const TopSpace = styled.div`
   height: 380px;
-`
+`;
