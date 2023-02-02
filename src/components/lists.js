c17a4b279b53e95ad86cb57a4ad9f9d990f1dc57
조우상 <woosang3078@hanmail.net>
2023-02-02 10:45:44 +0900

    리스트 조회 페이지 제목 스타일링

---
+++ src/components/lists.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function Lists() {
+    return (
+        <div>
+            <TopSpace />
+            <Title>Tensorflow.js 모델 기반 딥러닝 기능</Title>
+        </div>
+    )
+}
+
+const Title = styled.p`
+  font-size: 35px;
+  font-weight: 600;
+  text-align: center;
+`
+
+const TopSpace = styled.div`
+  height: 190px;
+`
