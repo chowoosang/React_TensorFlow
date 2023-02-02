e2f30baecf3b560761a5cb561f2bc81469003cb7
조우상 <woosang3078@hanmail.net>
2023-02-02 12:43:02 +0900

    메인 페이지 탭 제목 할당

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,12 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
+import useTitle from "./hooks/title";
 
 export default function Main() {
+  const titleUpdater = useTitle("불러오는 중...")
+  setTimeout(() => titleUpdater("DeepLearning Projects"))
+
   return (
     <div>
       <TopSpace />
