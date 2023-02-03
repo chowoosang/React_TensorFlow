2c66b95750f3e294b5833ae83e081832f5cf15ea
조우상 <woosang3078@hanmail.net>
2023-02-03 15:37:22 +0900

    반응형 헤더 스타일링

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,81 +4,85 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <>
-          <HeaderContainer>
-            <TopSpace />
-            <LogoContainer>
-                <LogoTitle onClick={() => navigate("/")}>Deep. Projects</LogoTitle>
-            </LogoContainer>
-            <ListContainer>
-                <Lists>
-                    <LinkStyle to="/image-identify">이미지 분류</LinkStyle>
-                </Lists>
-                <Lists>
-                    <LinkStyle to="/delete-background">배경 제거</LinkStyle>
-                </Lists>
-                <Lists>
-                    <LinkStyle to="/webcam-detection">웹캠 사물 탐지</LinkStyle>
-                </Lists>
-            </ListContainer>
-          </HeaderContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <HeaderContainer>
+        <TopSpace />
+        <LogoContainer>
+          <LogoTitle onClick={() => navigate("/")}>Deep. Projects</LogoTitle>
+        </LogoContainer>
+        <ListContainer>
+          <Lists>
+            <LinkStyle to="/image-identify">이미지 분류</LinkStyle>
+          </Lists>
+          <Lists>
+            <LinkStyle to="/delete-background">배경 제거</LinkStyle>
+          </Lists>
+          <Lists>
+            <LinkStyle to="/webcam-detection">웹캠 사물 탐지</LinkStyle>
+          </Lists>
+        </ListContainer>
+      </HeaderContainer>
+    </>
+  );
+};
 
 export default Header;
 
 const LinkStyle = styled(Link)`
-    color: black;
-    text-decoration: none;
-`
+  color: black;
+  text-decoration: none;
+`;
 
 const TopSpace = styled.div`
-    height: 25px;
-`
+  height: 25px;
+`;
 
 const Lists = styled.li`
-    list-style: none;
-    font-size: 18px;
-    margin-left: 100px;
-    transition: font-size 0.5s ease-in-out;
-    &:nth-child(1):hover {
-        font-size: 20px;
-    }
-    &:nth-child(2):hover {
-        font-size: 20px;
-    }
-    &:nth-child(3):hover {
-        font-size: 20px;
-    }
-`
+  list-style: none;
+  font-size: 18px;
+  margin-left: 100px;
+  transition: font-size 0.5s ease-in-out;
+  &:nth-child(1):hover {
+    font-size: 20px;
+  }
+  &:nth-child(2):hover {
+    font-size: 20px;
+  }
+  &:nth-child(3):hover {
+    font-size: 20px;
+  }
+`;
 
 const ListContainer = styled.ul`
   display: flex;
   justify-content: center;
   margin: 0;
   margin-top: -20px;
-`
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+  display: none;
+`;
 
 const LogoTitle = styled.span`
-    font-size: 22px;
-    font-weight: 600;
-    margin: 0;
-    cursor: pointer;
-`
+  font-size: 22px;
+  font-weight: 600;
+  margin: 0;
+  cursor: pointer;
+`;
 
 const LogoContainer = styled.div`
-    margin-left: 30px;
-`
+  margin-left: 30px;
+`;
 
 const HeaderContainer = styled.header`
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 80px;
-    border: 1px solid #DFDFDF;
-`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  border: 1px solid #dfdfdf;
+`;
