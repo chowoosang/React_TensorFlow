50c916db337ed85129cf1f65fa06354ad9eb9434
조우상 <woosang3078@hanmail.net>
2023-02-03 10:23:59 +0900

    헤더 컨테이너 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,5 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-    
+    return (
+        <>
+          <HeaderContainer>
+
+          </HeaderContainer>
+        </>
+    )
 }
+
+export default Header;
+
+const HeaderContainer = styled.header`
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 80px;
+`
