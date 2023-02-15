c63d7e646e053ff25918d5673a6d8e921c554286
조우상 <woosang3078@hanmail.net>
2023-02-15 14:07:56 +0900

    오류 해결

--- src/components/Detection/webcam.js
+++ src/components/Detection/webcam.js
@@ -1,64 +1,64 @@
 // Import dependencies
 import React, { useRef, useEffect } from "react";
-import  "@tensorflow/tfjs";
+import "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
-import '../../../src/App.css'
+import "../../../src/App.css";
 import { drawRect } from "./utilities";
 import useTitle from "../hooks/title";
 
 function Detection() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const titleUpdater = useTitle("불러오는 중...")
-  setTimeout(() => titleUpdater("웹캠 사물 탐지"))
+  const titleUpdater = useTitle("불러오는 중...");
+  setTimeout(() => titleUpdater("웹캠 사물 탐지"));
 
   const runCoco = async () => {
     const net = await cocossd.load();
     console.log("Handpose model loaded.");
-    
+
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
-  
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-     
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      
       const obj = await net.detect(video);
 
-      
       const ctx = canvasRef.current.getContext("2d");
-      drawRect(obj, ctx); 
+      drawRect(obj, ctx);
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(
+    () => {
+      runCoco();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
           style={{
             position: "absolute",
             marginLeft: "auto",
